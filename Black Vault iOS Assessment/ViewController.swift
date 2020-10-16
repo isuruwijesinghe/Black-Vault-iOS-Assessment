f6077c339b2afadae5b9e7b6343383502177f515
isuru <isuru@wadinternational.net>
2020-10-16 16:51:53 +0530

    init commit

---
+++ Black Vault iOS Assessment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Black Vault iOS Assessment
+//
+//  Created by Isuru Wijesinghe on 10/16/20.
+//  Copyright © 2020 Isuru Wijesinghe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
