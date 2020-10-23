eb1daebfa016f5c87f6842e6696867186c2483ef
isuru <isuru@wadinternational.net>
2020-10-24 00:39:34 +0530

    removed the table view and added the collection view

--- Black Vault iOS Assessment/ViewController.swift
+++ Black Vault iOS Assessment/ViewController.swift
@@ -9,14 +9,14 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var CurrentDateLabel: UILabel!
     
     @IBOutlet weak var LoginButton: UIButton!
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var collectionView: UICollectionView!
     
-    let basedUrl: String = "https://temper.works/api/v1/contractor/shifts?dates=2020-10-23"
+    let basedUrl: String = "https://temper.works/api/v1/contractor/shifts?dates=2020-10-24"
     var objArray : [NSDictionary] = []
     
     let getLocation = GetLocation()
@@ -40,67 +40,71 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         CurrentDateLabel.text = dateString
         
         //set cell and row height
-        tableView.rowHeight = 320;
-        let nib = UINib(nibName: "TableViewCell", bundle: nil)
-        tableView.register(nib, forCellReuseIdentifier: "TableViewCell")
+        collectionView.register(UINib(nibName: "JobsCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "JobsCollectionViewCell")
         
+
         getData(from: basedUrl)
         
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return objArray.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return objArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "JobsCollectionViewCell", for: indexPath) as! JobsCollectionViewCell
         cell.JobNameView.text = objArray[indexPath.row].value(forKey: "title") as? String
-        
-        cell.selectionStyle = .none
-        
+                
+                
         let shifts = objArray[indexPath.row].value(forKeyPath: "shifts") as! [NSDictionary]
-        cell.JobsRateView.text = "   $ \(shifts[0].value(forKey: "earnings_per_hour") ?? "0")"
-        
+        cell.JobsRateView.text = "   $ \(shifts[0].value(forKey: "earnings_per_hour") ?? "0")  "
+                
         cell.JobsTimeView.text = "\(shifts[0].value(forKey: "start_time") ?? "0") - \(shifts[0].value(forKey: "end_time") ?? "0")"
-        
+                
         let job_image = objArray[indexPath.row].value(forKey: "photo") ?? "https://tmpr-photos.ams3.digitaloceanspaces.com/hero/153621.jpg"
-        
+                
         let job_lat = objArray[indexPath.row].value(forKeyPath: "location.lat")
         let job_lng = objArray[indexPath.row].value(forKeyPath: "location.lng")
-        
+                
         let location2:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: Double((job_lng as! NSString).doubleValue), longitude: Double((job_lat as! NSString).doubleValue))
 
-        
-        //setting the distance and job category 
+                
+        //setting the distance and job category
         getLocation.run {
             if let location = $0 {
-//                print("location = \(location.coordinate.latitude) \(location.coordinate.longitude)")
-                 let coordinate1 = CLLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-                let coordinate2 = CLLocation(latitude: location2.latitude, longitude: location2.longitude)
+        //                print("location = \(location.coordinate.latitude) \(location.coordinate.longitude)")
+            let coordinate1 = CLLocation(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            let coordinate2 = CLLocation(latitude: location2.latitude, longitude: location2.longitude)
 
-                 let distanceInMeters = coordinate1.distance(from: coordinate2)
-                print("location ---->",round(distanceInMeters/1000))
-                cell.JobsDistanceView.text = "\(self.objArray[indexPath.row].value(forKeyPath: "job_category.description") ?? "") . \(round(distanceInMeters/1000))KM"
+            let distanceInMeters = coordinate1.distance(from: coordinate2)
+            print("location ---->",round(distanceInMeters/1000))
+            cell.JobsDistanceView.text = "\(self.objArray[indexPath.row].value(forKeyPath: "job_category.description") ?? "") . \(round(distanceInMeters/1000))KM"
             } else {
                 print("Get Location failed \(self.getLocation.didFailWithError)")
+                }
             }
-        }
-        
-        
-        let url = URL(string: job_image as! String)!
-        downloadImage(from: url)
-        getImageData(from: url) { data, response, error in
+                
+                
+            let url = URL(string: job_image as! String)!
+            downloadImage(from: url)
+            getImageData(from: url) { data, response, error in
             guard let data = data, error == nil else { return }
 //            print(response?.suggestedFilename ?? url.lastPathComponent)
 //            print("Download Finished")
             DispatchQueue.main.async() { [weak self] in
-                cell.JobsImageView.contentMode = .scaleAspectFill
-                cell.JobsImageView.image = UIImage(data: data)
+            cell.JobsImageView.contentMode = .scaleAspectFill
+            cell.JobsImageView.image = UIImage(data: data)
+                }
             }
-        }
         
         return cell
     }
+    
+
     
     func getImageData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
         URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
@@ -128,10 +132,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                  let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
 //                    print(json)
                     let applicationStateJson = json["data"] as? NSDictionary
-                    self.objArray = (applicationStateJson?["2020-10-23"] as? [NSDictionary])!
+                    self.objArray = (applicationStateJson?["2020-10-24"] as? [NSDictionary])!
                                     
                     DispatchQueue.main.async{
-                        self.tableView.reloadData()
+                        //reload table
+                        self.collectionView.reloadData()
                     }
                 } catch {
                     print(error.localizedDescription)
---
+++ Black Vault iOS Assessment/Views/JobsCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  JobsCollectionViewCell.swift
+//  Black Vault iOS Assessment
+//
+//  Created by Isuru Wijesinghe on 10/23/20.
+//  Copyright © 2020 Isuru Wijesinghe. All rights reserved.
+//
+
+import UIKit
+
+class JobsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var JobsTimeView: UILabel!
+    @IBOutlet weak var JobsDistanceView: UILabel!
+    @IBOutlet weak var JobsRateView: UILabel!
+    @IBOutlet weak var JobsImageView: UIImageView!
+    @IBOutlet weak var JobNameView: UILabel!
+    
+    override func awakeFromNib() {
+            super.awakeFromNib()
+            // Initialization code
+
+            self.JobsImageView.layer.masksToBounds = true
+            self.JobsImageView.layer.cornerRadius = self.JobsImageView.frame.width/19.0
+
+            JobsRateView?.layer.masksToBounds = true
+    //        JobsRateView?.layer.cornerRadius = 20.0
+    //        JobsRateView?.roundCorners(corners: [.topLeft, .topRight], radius: 30)
+            //MARK:- Corner Radius of only two side of UIViews
+            self.roundCorners(view: JobsRateView, corners: [.topLeft], radius: 10.0)
+
+        }
+
+    //MARK:- Corner Radius of only two side of UIViews
+    func roundCorners(view :UIView, corners: UIRectCorner, radius: CGFloat){
+            let path = UIBezierPath(roundedRect: view.bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+            let mask = CAShapeLayer()
+            mask.path = path.cgPath
+            view.layer.mask = mask
+    }
+    
+}
